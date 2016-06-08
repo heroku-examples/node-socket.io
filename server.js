cabdce931bb3618eeb7b765e0fd646af7b58a3e2
Hunter Loftis <hunter@hunterloftis.com>
2016-06-08 11:29:42 -0400

    server push

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  socket.on('getTime', () => {
-    socket.emit('time', new Date().toTimeString());
-  });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
