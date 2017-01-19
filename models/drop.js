b8a29878bd8e8b9e1588b6d614a99138fbe8158f
Benjamin Adams <ben@benadams.co>
2017-01-19 15:30:46 -0600

    merged

--- models/drop.js
+++ models/drop.js
@@ -4,50 +4,50 @@ const lodash = require('lodash');
 const Battle = require('./battle.js');
 
 const schema = new mongoose.Schema({
-	denaItemId: {type: String},
-	battle: {type: mongoose.Schema.Types.ObjectId, ref: 'Battle'},
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    denaItemId: { type: String },
+    qty: { type: Number },
+    rarity: { type: Number },
+    battle: { type: mongoose.Schema.Types.ObjectId, ref: 'Battle' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 schema.pre('save', function (next) {
-
-	Battle
-	.update( {_id: this.battle}, { $addToSet: {drops: this._id } } )
-	.then(( (battles) => { next(); }))
-	.error(( (err) => next(err) ));
+    Battle
+        .update({ _id: this.battle }, { $addToSet: { drops: this._id } })
+        .then(((battles) => { next(); }))
+        .error(((err) => next(err)));
 });
 
 schema.pre('save', function (next) {
-
-	mongoose.model('User')
-	.update( {_id: this.user}, { $addToSet: {drops: this._id } } )
-	.then(( (users) => { next(); }))
-	.error(( (err) => next(err) ));
+    mongoose.model('User')
+        .update({ _id: this.user }, { $addToSet: { drops: this._id } })
+        .then(((users) => { next(); }))
+        .error(((err) => next(err)));
 });
 
 schema.post('save', function (drop) {
-	mongoose.model('Drop', schema)	
-	.find({battle: drop.battle})
-	.then(function(drops) {
-		return [drops, Battle.findById(drop.battle)]
-	})
-	.spread(function(drops, battle) {
-		///TODO: totally not transactionally safe
-		battle.dropRates = battle.dropRates || {};
-		battle.dropRates[drop.denaItemId] = battle.dropRates[drop.denaItemId] || {};
-		for(var i in battle.dropRates) {
-			if(i) {
-				battle.dropRates[i] = {
-					total: drops.length,
-					hits: lodash.filter(drops, (d) => { return i.toString() == (d.denaItemId||"").toString() }).length
-				};
-			}
-		}
+    mongoose.model('Drop', schema)
+        .find({ battle: drop.battle })
+        .then(function (drops) {
+            return [drops, Battle.findById(drop.battle)]
+        })
+        .spread(function (drops, battle) {
+            ///TODO: totally not transactionally safe
+            battle.dropRates = battle.dropRates || {};
+            battle.dropRates[drop.denaItemId] = battle.dropRates[drop.denaItemId] || {};
+            for (var i in battle.dropRates) {
+                if (i) {
+                    battle.dropRates[i] = {
+                        total: drops.length,
+                        hits: lodash.filter(drops, (d) => { return i.toString() == (d.denaItemId || "").toString() }).length
+                    };
+                }
+            }
 
-		battle.dropRates[drop.denaItemId].rate = (battle.dropRates[drop.denaItemId].hits * 1.0 ) / (battle.dropRates[drop.denaItemId].total * 1.0);
+            battle.dropRates[drop.denaItemId].rate = (battle.dropRates[drop.denaItemId].hits * 1.0) / (battle.dropRates[drop.denaItemId].total * 1.0);
 
-		return Battle.update({_id: drop.battle}, {dropRates: battle.dropRates});
-	});
+            return Battle.update({ _id: drop.battle }, { dropRates: battle.dropRates });
+        });
 });
 
 module.exports = mongoose.model('Drop', schema);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
 try {
-  require('dotenv').config();
-} catch(e) {
-  // ignore it
+    require('dotenv').config();
+} catch (e) {
+    // ignore it
 }
 
 const express = require('express');
@@ -22,61 +22,49 @@ require('./config/mongoose.js').setup(mongoose);
 const automation = require('./automation.js');
 const User = require('./models/user.js');
 
-
-
-
-
 // automation.begin();
-
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-
-
-
 const server = express()
-	.use(bodyParser.json())
-	.use(bodyParser.json(bodyParser.urlencoded({ extended: true })))
-  .use(express.static(path.join(__dirname, 'public'), {
-    maxAge: process.env.NODE_ENV == 'production' ? 86400000 : 0
-  }))
-	.get('/', function (req, res) {
-    res.sendFile(INDEX);
-  })
-  .post('/tick', function (req, res) {
-    res.send('GET request to the homepage');
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-  
+    .use(bodyParser.json())
+    .use(bodyParser.json(bodyParser.urlencoded({ extended: true })))
+    .use(express.static(path.join(__dirname, 'public'), {
+        maxAge: process.env.NODE_ENV == 'production' ? 86400000 : 0
+    }))
+    .get('/', function (req, res) {
+        res.sendFile(INDEX);
+    })
+    .post('/tick', function (req, res) {
+        res.send('GET request to the homepage');
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  
-  socket.on('disconnect', () => {
-  	console.log('Client disconnected');
-  });
+    console.log('Client connected');
 
-  socket.on('signin', (data, fn) => {
-    User.findOne({'dena.sessionId': data.sessionId})
-    .then((user) => {
-      console.log(user);
-      if(user) {
-        return Promise.resolve(user);
-      } else {
-        return User.create({'dena.sessionId': data.sessionId});
-      }
-    })
-    .then((user) => {
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 
-      return User.update({'dena.sessionId': data.sessionId}, {phone: data.phone, email: data.email});
-    })
-  });
+    socket.on('signin', (data, fn) => {
+        User.findOne({ 'dena.sessionId': data.sessionId })
+            .then((user) => {
+                console.log(user);
+                if (user) {
+                    return Promise.resolve(user);
+                } else {
+                    return User.create({ 'dena.sessionId': data.sessionId });
+                }
+            })
+            .then((user) => {
+                return User.update({ 'dena.sessionId': data.sessionId }, { phone: data.phone, email: data.email });
+            })
+    });
 });
-
-
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 setInterval(() => { User.schema.statics.doDropCheck(io) }, 3000);
